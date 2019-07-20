40f3c473f4341fa6ac7b2b8c58a2aeb9836ae88e
Theo Taylor <theotaylor@taylord.tech>
2019-07-20 03:53:37 -0400

    Make code more similar to current lib

--- src/behavior-reanimated.js
+++ src/behavior-reanimated.js
@@ -1,104 +1,151 @@
 import * as React from 'react';
 import Animated, {Easing} from "react-native-reanimated";
 
-import {parallel, sequence} from "./ReAnimated";
+import {sequence} from "./ReAnimated";
 
 const {
+  Extrapolate,
   Value,
   concat,
   timing,
 } = Animated;
 
-const defaultState = {scale: 1, rotate: '0deg', opacity: 1, translateX: 0, translateY: 0};
+const PROPS = [
+  { prop: 'opacity', default: 1 },
+  { prop: 'rotate', default: '0deg', transform: true },
+  { prop: 'scale', default: 1, transform: true },
+  { prop: 'translateX', default: 0, transform: true },
+  { prop: 'translateY', default: 0, transform: true },
+];
+
+const defaultConfig = { type: 'spring', onComplete: ()=> console.log('Animation completed') };
 
 class Behaviour extends React.PureComponent {
+  ref = React.createRef();
+  
+  static defaultProps = {
+    clamp: false,
+    clearStyleProps: false,
+    config: defaultConfig,
+    currentState: 0,
+    disabled: false,
+    initialState: 0,
+    skipProps: [],
+    skipStyleProps: [],
+    state: [{}, {}],
+    style: {},
+    styleProps: [],
+    unmounted: false,
+  };
+  
   constructor(props) {
     super(props);
-    const {state, currentState, initialState} = props;
-    const initialValues = state[initialState || currentState || 0];
-    this.scale = new Value(initialValues.scale || defaultState.scale);
-    this.rotate = new Value((initialValues.rotate) || (defaultState.rotate));
-    this.transX = new Value(initialValues.translateX || defaultState.translateX);
-    this.transY = new Value(initialValues.translateY || defaultState.translateY);
-    this.opacity = new Value(initialValues.opacity || defaultState.opacity);
+    const {initialState} = props;
     
-    this.animatedStates = [];
+    this.nativeDriver = new Value(initialState);
     
-    state.forEach((item, index)=> {
-      if (index === 0){
-        this.animatedStates.push({...defaultState, ...state[0]});
-      } else {
-        this.animatedStates.push({...this.animatedStates[index - 1], ...state[index]});
-      }
-    });
+    this.nativeStyles = {};
+    
+    this.setNativeStyles();
+    
   }
   
-  run = (nextProps) => {
-    const {currentState, config} = this.props;
-    const {currentState: nextCurrentState,} = nextProps;
-    let duration = config.duration || 1000;
+  setNativeStyles = ()=> {
+    const {
+      clamp,
+      clearStyleProps,
+      keys,
+      skipStyleProps = [],
+      state,
+      style,
+      styleProps = [],
+    } = this.props;
     
-    const delta = Math.abs(currentState - nextCurrentState);
+    const inputRange =
+      keys ||
+      Array(state.length)
+        .fill()
+        .map((_, index) => index);
     
-    const animations = [];
-    duration = duration / delta;
-    const includedStates = [];
-    
-    /* set which states we need to iterate through */
-    if (nextCurrentState > currentState) { // increasing
-      for (let i = currentState; i < nextCurrentState; i++) {
-        includedStates.push(i + 1);
-      }
-    } else if (nextCurrentState < currentState) { // decreasing
-      for (let i = currentState; i > nextCurrentState; i--) {
-        includedStates.push(i - 1);
-      }
+    if (inputRange.length === 1) {
+      inputRange.push(1);
+      state.push({})
     }
     
-    /* create a parallel animation for each state then push to the animations array to be animated in sequence */
-    for (let i = 0; i < includedStates.length; i++) {// runs for each parallel animation
-      const stateToPush = this.animatedStates[includedStates[i]];
-      
-      const config = (prop) => {
-        return {
-          duration,
-          toValue: stateToPush[prop],
-          easing: Easing.inOut(Easing.ease),
-        };
-      };
-      
-      const scaleAnim = timing(this.scale, config('scale'));
-      const rotateAnim = timing(this.rotate, config('rotate'));
-      const transXAnim = timing(this.transX, config('translateX'));
-      const transYAnim = timing(this.transY, config('translateY'));
-      const opacityAnim = timing(this.opacity, config('opacity'));
-      const pAnimations = parallel([scaleAnim, rotateAnim, opacityAnim, transXAnim, transYAnim]);
-      animations.push(pAnimations);
-    }
+    const getRange = (prop, defaultValue) => {
+      return state.reduce((range, currentState, index) => {
+        const prevState = range[index - 1];
+        const val = currentState[prop] || currentState[prop] === 0
+          ? currentState[prop]
+          : prevState || prevState === 0
+            ? prevState
+            : style[prop] || style[prop] === 0
+              ? style[prop]
+              : defaultValue;
+        range.push(
+          // Very weird issue with rotate, have to remove 'deg'
+          prop === 'rotate'? Number(val.toString().replace('deg', '')) : val
+        );
+        return range
+      }, []);
+    };
     
-    sequence(animations).start(() => {
+    const addProp = (prop, defaultValue) => {
+      return this.nativeDriver.interpolate({
+        inputRange,
+        outputRange: getRange(prop, defaultValue),
+        extrapolate: clamp ? Extrapolate.CLAMP : undefined,
+      });
+    };
+    
+    const defaultStyleProps = clearStyleProps ? [] : PROPS;
+    
+    const allStyleProps = [...defaultStyleProps, ...styleProps];
+    
+    allStyleProps.forEach(({ prop, default: defaultValue, transform }) => {
+      if (!skipStyleProps.includes(prop)) {
+        // Very weird issue with rotate, have to add 'deg'
+        const val = prop === 'rotate'? concat(addProp(prop, defaultValue), 'deg') : addProp(prop, defaultValue);
+        if (transform) {
+          this.nativeStyles.transform = [
+            ...(this.nativeStyles.transform || []),
+            { [prop]: val },
+          ]
+        } else {
+          this.nativeStyles[prop] = val
+        }
+      }
+    });
+  };
+  
+  goTo = (nextCurrentState)=> {
+    const {
+      config,
+    } = this.props;
+    
+    this.nativeDriverConfig = {
+      duration: config.duration,
+      toValue: nextCurrentState,
+      easing: Easing.inOut(Easing.ease),
+    };
+    this.nativeDriverAnim = timing(this.nativeDriver, this.nativeDriverConfig);
+    sequence([this.nativeDriverAnim]).start(() => {
+      defaultConfig.onComplete();
     });
   };
   
   componentWillReceiveProps(nextProps, nextContext) {
     const {currentState} = this.props;
     const {currentState: nextCurrentState,} = nextProps;
-    if (currentState !== nextCurrentState) this.run(nextProps); // Only run if current state has been updated
+    if (currentState !== nextCurrentState) this.goTo(nextCurrentState); // Only run if current state has been updated
   }
   
   render() {
     const {children, style} = this.props;
     return (
       <Animated.View
-        style={[style, {
-          opacity: this.opacity,
-          transform: [
-            {rotate: concat(this.rotate, 'deg')},
-            {scale: this.scale},
-            {translateX: this.transX},
-            {translateY: this.transY}
-          ]
-        }]}>
+        ref={this.ref}
+        style={[style, this.nativeStyles]}>
         {children}
       </Animated.View>
     );
